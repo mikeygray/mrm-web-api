c35bac0562ac07fad9d7b68e07d43e18cd956c35
Mikey Gray <michael.r.a.gray@gmail.com>
2018-04-15 14:53:58 +0100

    Configuration.Seed() now seeds datasource
    FINALLY FINALLY getting back to this after an awkward never ending week!
    
    Anyway, created a Model for the Product object and a Seed method to populate the datasource. It does this by embedding products.json as a resource and reading it internally. Possibly not the best way to do it depending on how often products.json might change or how big it could be. One assumes seeding (or even re-seeding) would not be a frequent occurance and as it stands it doesn't but a great wait on package size.

---
+++ MRMWebAPI/Migrations/Configuration.cs
@@ -0,0 +1,54 @@
+namespace MRMWebAPI.Migrations
+{
+    using MRMWebAPI.Models;
+    using Newtonsoft.Json.Linq;
+    using System.Data.Entity.Migrations;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Reflection;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ProductServiceContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(ProductServiceContext context)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            string jsonRaw = string.Empty;
+
+            try
+            {
+                /*
+                 * JSON data file included as an embedded resource and then read. Resource location/name saved in Resource.resx
+                 * Not suitable for large seeds? 
+                 */
+                using (Stream stream = assembly.GetManifestResourceStream(Properties.Resources.ProductsEmbeddedResourceName))
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    jsonRaw = reader.ReadToEnd();
+                }
+
+                var jsonToken = JToken.Parse(jsonRaw);
+                var productsArray = jsonToken.ToObject<Product[]>();
+                var count = 0;
+
+                foreach (Product product in productsArray)
+                {
+                    product.Id = count++;
+                    context.Products.AddOrUpdate(x => x.Id, product);
+                    //Debug.WriteLine("{0} | {1} | {2} | {3}", product.Id, product.Name, product.Category, product.Description);
+                }
+            }
+            catch (System.Exception ex)
+            {
+                /*
+                 * Could be overloaded/replaced to write to log, etc.
+                 */
+                Debug.WriteLine("Error Parsing JSON and Seeding DB: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ MRMWebAPI/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace MRMWebAPI.Models
+{
+    public class Product
+    {
+        private int _id;
+        private string _name;
+        private string _description;
+        private string _category;
+        
+        public int Id { get => _id; set => _id = value; }
+        public string Name { get => _name; set => _name = value; }
+        public string Description { get => _description; set => _description = value; }
+        public string Category { get => _category; set => _category = value; }
+
+    }
+}
